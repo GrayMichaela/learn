fc486d130b5805b6a7f148e0f5e4ddad52eb8f69
XiaoYueTeng <31399275+GrayMichaela@users.noreply.github.com>
2019-05-11 15:11:57 +0800

    add echarts

---
+++ front/pc/src/components/chart/index.vue
@@ -0,0 +1,43 @@
+<style lang="less" scoped>
+    @import url('./index.less');
+</style>
+<template>
+    <div class="chart" ref="chart"></div>
+</template>
+<script>
+    import echarts from 'echarts'
+    export default {
+        name: 'VChart',
+        props: {
+            loading: {
+                require: true,
+                type: Boolean,
+                default: false
+            },
+            config: {
+                require: true,
+                type: Object,
+                default: cxknmsl => ({})
+            }
+        },
+        computed: {
+            chart() {
+                return echarts.init(this.$refs.chart);
+            },
+        },
+        mounted() {
+            this.loadChart();
+        },
+        methods: {
+            loadChart(config=false) {
+                this.chart.setOption(this.config,config);
+            }
+        },
+        watch: {
+            'loading'(v) {
+                v ? this.chart.showLoading() : this.chart.hideLoading();
+            },
+            'config': 'loadChart'
+        }
+    }
+</script>
--- front/pc/src/views/home/index.vue
+++ front/pc/src/views/home/index.vue
@@ -1,26 +1,93 @@
+<style lang="less" scoped>
+  @import url('./index.less');
+</style>
+
 <template>
-  <div>
-    <span>{{test.count}}</span>
-    <button @click="what">clicke Me</button>
-    <el-checkbox v-model="checked">备选项</el-checkbox>
+  <div class="sdiv">
+    <v-chart :loading="loading" :config="bar" />
   </div>
 </template>
 <script>
-export default {
-  data() {
-    return {};
-  },
-  computed: {
-    test() {
-      return this.$store.state;
-    }
-  },
-  methods: {
-    what() {
-      this.$store.dispatch("test", {
-        q: 1
-      });
-    }
+  export default {
+    data() {
+      return {
+        x: [],
+        y: [],
+        loading: false,
+      }
+    },
+    computed: {
+      bar() {
+        return {
+          grid: {
+            top: '1%',
+            right: '1%',
+            left: '1%',
+            bottom: '1%',
+            containLabel: true
+          },
+          title: {
+            text: 'test',
+            subtext: 'qqn',
+            show: false
+          },
+          legen: {
+            show: true,
+            top: '5%'
+          },
+          tooltip: {},
+          dataset: {
+            // 提供一份数据。
+            dimensions: this.x,
+            source: this.y
+          },
+          // 声明一个 X 轴，类目轴（category）。默认情况下，类目轴对应到 dataset 第一列。
+          xAxis: {},
+          // 声明一个 Y 轴，数值轴。
+          yAxis: {
+            type: 'category'
+          },
+          // 声明多个 bar 系列，默认情况下，每个系列会自动对应到 dataset 的每一列。
+          series: [{
+              type: 'bar'
+            },
+            {
+              type: 'bar'
+            },
+            {
+              type: 'bar'
+            }
+          ]
+        }
+      }
+    },
+    mounted() {
+      this.loading = true;
+      setTimeout(() => {
+        this.x.push('product', '2015', '2016');
+        this.y.push({
+          product: 'Matcha Latte',
+          '2015': 43.3,
+          '2016': 85.8,
+          '2017': 93.7
+        }, {
+          product: 'Milk Tea',
+          '2015': 83.1,
+          '2016': 73.4,
+          '2017': 55.1
+        }, {
+          product: 'Cheese Cocoa',
+          '2015': 86.4,
+          '2016': 65.2,
+          '2017': 82.5
+        }, {
+          product: 'Walnut Brownie',
+          '2015': 72.4,
+          '2016': 53.9,
+          '2017': 39.1
+        });
+        this.loading = false;
+      }, 1100);
+    },
   }
-};
 </script>
