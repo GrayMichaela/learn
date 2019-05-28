2027a9a4ca3e8caeefd2d70719ef2b5b580e6b53
tyx <443827207@qq.com>
2019-05-28 13:21:19 +0800

    移除移动端所有文件

--- front/mobile/src/router.js
+++
@@ -1,9 +0,0 @@
-import Vue from"vue";
-import VueRouter from"vue-router";
-import home from"./modules/home/index";
-export default new VueRouter({
-	base:"/"
-	,mode:"hash"
-	,routes:[home.route]
-});
-Vue.use(VueRouter);
--- front/mobile/src/store.js
+++
@@ -1,9 +0,0 @@
-import Vue from"vue";
-import Vuex from"vuex";
-Vue.use(Vuex);
-import home from"./modules/home/index";
-export default new Vuex.Store({
-	strict:true
-	,namespace:true
-	,modules:{home:home.store}
-});
